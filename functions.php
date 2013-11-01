aedd23421245d99a986dbc1b8a427f2c9c2ff6c2
= <=>
2013-11-01 14:43:18 -0400

    executive options

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@ require_once('nhp-options.php');
 
 /**
  * Set the content width based on the theme's design and stylesheet.
- */
+ */ 
 if ( ! isset( $content_width ) )
 	$content_width = 750; /* pixels */
 

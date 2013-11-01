e836eb16a1ea9b891298c47d998acd86907644c8
mbejda <mbejda@live.com>
2013-11-01 12:04:40 -0400

    rounded products

--- functions.php
+++ functions.php
@@ -168,3 +168,56 @@ require get_template_directory() . '/includes/jetpack.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/includes/bootstrap-wp-navwalker.php';
+
+
+
+
+remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 10);
+add_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 10);
+
+/**
+ * WooCommerce Loop Product Thumbs
+ **/
+
+ if ( ! function_exists( 'woocommerce_template_loop_product_thumbnail' ) ) {
+
+	function woocommerce_template_loop_product_thumbnail() {
+		echo woocommerce_get_product_thumbnail();
+	} 
+ }
+
+
+/**
+ * WooCommerce Product Thumbnail
+ **/
+ if ( ! function_exists( 'woocommerce_get_product_thumbnail' ) ) {
+	
+	function woocommerce_get_product_thumbnail( $size = 'shop_catalog', $placeholder_width = 0, $placeholder_height = 0  ) {
+		global $post, $woocommerce;
+
+		if ( ! $placeholder_width )
+			$placeholder_width = $woocommerce->get_image_size( 'shop_catalog_image_width' );
+		if ( ! $placeholder_height )
+			$placeholder_height = $woocommerce->get_image_size( 'shop_catalog_image_height' );
+			
+			$output = '<div class="center-product">';
+
+			if ( has_post_thumbnail() ) {
+				
+				$output .= get_the_post_thumbnail( $post->ID, $size ); 
+				
+			} else {
+			
+				$output .= '<img src="'. woocommerce_placeholder_img_src() .'" alt="Placeholder" width="' . $placeholder_width . '" height="' . $placeholder_height . '" />';
+			
+			}
+			
+			$output .= '</div>';
+			
+			return $output;
+	}
+ }
+?>
+
+
+
--- woocommerce/single-product/related.php
+++ woocommerce/single-product/related.php
@@ -31,7 +31,7 @@ $woocommerce_loop['columns'] 	= $columns;
 
 if ( $products->have_posts() ) : ?>
 
-	<div class="related products related">
+	<div class="related products related col-3">
 
 		<h2><?php _e( 'Related Products', 'woocommerce' ); ?></h2>
 
@@ -47,6 +47,6 @@ if ( $products->have_posts() ) : ?>
 
 	</div>
 
-<?php echo "test"; endif;
+<?php endif;
 
 wp_reset_postdata();
